10cab6263ebd15d82ab08ba1f5c4666744260272
Christian Bastidas <cris931201@gmail.com>
2024-06-27 17:04:11 +0200

    Update DomainTest.js

--- DomainTest.js
+++ DomainTest.js
@@ -1,4 +1,4 @@
-// Configuración de la cuenta regresiva
+c// Configuración de la cuenta regresiva
 let countdownElement = document.getElementById('countdown');
 let countdownValue = 10;
 
@@ -8,7 +8,7 @@ let countdownInterval = setInterval(() => {
 
     if (countdownValue <= 0) {
         clearInterval(countdownInterval);
-        // Redirigir a un nuevo dominio
+        // Redirect to a new domain
         window.location.href = 'https://www.myaffiliates.com/';
     }
 }, 1000);
